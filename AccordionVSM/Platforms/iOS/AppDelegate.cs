68dd7a721225587017e6f1da1725f5bcbead5620
AnanthaLakshmiKannan <ananthalakshmi.k@syncfusion.com>
2023-06-19 11:33:09 +0530

    UG DOCUMENTATION 831668 - Added sample for Accordion VisualStateManager

---
+++ AccordionVSM/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AccordionVSM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
